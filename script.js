3a3162ffabfafb3b7872ddda092db3860be09cd6
torolero <42136673+Torolero@users.noreply.github.com>
2026-06-03 09:19:57 -0700

    Add object and operate function
    Create an object for mapping operators wrapped in quotes to functions
    for each operator. Then the operate function will return the result by
    calling a function with the values provided.
    Please enter the commit message for your changes. Lines starting

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const divide = (a, b) => (b !== 0 ? a / b : "Cannot divide by zero");
 
+let firstNumber;
+let operator;
+let secondNumber;
+
+const operations = {
+  '+': add,
+  '-': subtract,
+  '*': multiply,
+  '/': divide
+};
+
+const operate = (a, op, b) => {
+    return operations[op] ? operations[op](a, b) : "Invalid operator";
+};
+
+
+console.log(operate(2, '*', 3));
