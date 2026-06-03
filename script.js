2b555027aac2ef52fd7c7be07c6cee11edd5a110
torolero <42136673+Torolero@users.noreply.github.com>
2026-06-03 11:38:22 -0700

    Store button presses on first var
    This will add event listeners to each button and store their text
    content on the first var.

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ const operations = {
 const operate = (a, op, b) => {
     return operations[op] ? operations[op](a, b) : "Invalid operator";
 };
+
+
+
+const buttons = document.querySelectorAll('button');
+const updateFirstNumber = (num) => {(firstNumber == undefined) ? firstNumber = num : firstNumber = String(firstNumber + num)
+
+};
+
+//If this is the first number, replace undefine with the number 
+const display = document.querySelector('.display-input');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        updateFirstNumber(button.textContent);
+        display.textContent = firstNumber;
+    });
+});
+
+// buttons.addEventListener("click", (event) => {updateFirstNumber(buttons.textContent)});
+
